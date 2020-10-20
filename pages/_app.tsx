bf274bdaa0b480444b8002beb0de3b534a4ceaa4
三空 <sankong.shao@tuya.com>
2020-10-21 01:16:12 +0800

    new

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/app.scss";
+// import { Provider } from "react-redux";
+// import { useStore } from "../store/store";
+// This default export is required in a new `pages/_app.js` file.
+
+export default function App({ Component, pageProps }: any) {
+  // const store = useStore(pageProps.initialReduxState);
+
+  return (
+    // <Provider store={store}>
+      <Component {...pageProps} />
+    // </Provider>
+  );
+}
