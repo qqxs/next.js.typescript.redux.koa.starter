d07426bd7e39259c6ffff4d5e17ecb3d1e9cc952
三空 <sankong.shao@tuya.com>
2020-10-27 01:28:57 +0800

    useEffect

--- pages/todolist.tsx
+++ pages/todolist.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { NextPage } from "next";
+import React, { useEffect } from "react";
+import { NextPage, GetStaticProps } from "next";
 import { useSelector, useDispatch } from "react-redux";
 import styles from "@styles/pages/todolist.module.scss";
 import { IStoreStateType, ITodo } from "@store/types";
@@ -9,15 +9,22 @@ import { todosToggleComplateAction } from "@store/actions/todos.action";
 import Input from "@components/pages/todolist/Input";
 
 const TodoList: NextPage = props => {
+  console.log(props);
+
   const todos: ITodo[] = useSelector((state: IStoreStateType) => {
     return state.todos;
   });
 
   const dispatch = useDispatch();
-
   const handleToggleStatus = (id: number) => {
     dispatch(todosToggleComplateAction(id));
   };
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/rich-harris")
+      .then(res => res.json())
+      .then(res => console.log(res));
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -43,6 +50,20 @@ const TodoList: NextPage = props => {
       </main>
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  console.log(context);
+  const res = await fetch("https://api.github.com/users/rich-harris");
+  const posts = await res.json();
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts
+    }
+  };
 };
 
 export default TodoList;
