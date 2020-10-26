5534093a249f6077aa2db72e3f740ef31f918058
三空 <sankong.shao@tuya.com>
2020-10-27 01:31:53 +0800

    添加getInitialProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,62 @@
 import { Provider } from "react-redux";
-import { AppProps } from "next/app";
-import { useStore } from "../store";
+import { NextComponentType } from "next";
+import { AppContext, AppProps } from "next/app";
+import { useStore, initializeStore } from "../store";
 import "../styles/app.scss";
+import { todosAddAction } from "@store/actions/todos.action";
 // This default export is required in a new `pages/_app.js` file.
 
-export default function AppCom({
-  Component,
-  pageProps
-}: AppProps): JSX.Element {
-  const store = useStore(pageProps.initialReduxState);
+export interface ModifiedAppInitialProps<A = { [key in string]: string }> {
+  appProps: A;
+}
+
+export interface ExtendedAppProps<
+  P = { [key in string]: string },
+  A = { [key in string]: string }
+> extends AppProps<P>,
+    ModifiedAppInitialProps<A> {}
+
+const AppCom: NextComponentType<
+  AppContext,
+  ModifiedAppInitialProps,
+  ExtendedAppProps
+> = ({ Component, pageProps, appProps }) => {
+  const store = useStore(appProps.initialReduxState);
+
+  // console.log("Component", Component);
+  // console.log("pageProps", pageProps);
+  console.log("appProps", appProps);
 
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Component {...appProps} {...pageProps} />
     </Provider>
   );
-}
+};
+
+AppCom.getInitialProps = async (context: any) => {
+  const reduxStore = initializeStore();
+
+  // const { store, req, pathname, query, res } = context.ctx;
+
+  const { dispatch } = reduxStore;
+
+  // console.log("getServerSideProps", store, req, pathname, query, res);
+
+  dispatch(
+    todosAddAction({
+      id: 0,
+      completed: false,
+      text: "this is server",
+      timer: new Date().getTime() + ""
+    })
+  );
+
+  return {
+    appProps: {
+      initialReduxState: reduxStore.getState()
+    }
+  };
+};
+
+export default AppCom;
