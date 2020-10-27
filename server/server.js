fdcc6a6c0bbdd586e52e6cbb2475ec78197a6cda
三空 <sankong.shao@tuya.com>
2020-10-27 22:28:46 +0800

    更新

--- server/server.js
+++ server/server.js
@@ -7,23 +7,13 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-// const RouterApi = require("./router/api");
-
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
 
-  // api
-  // router.use("/api", RouterApi);
-  console.log("app start");
-  // next
-  router.all("*", async ctx => {
+  server.use(async (ctx, next) => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
-  });
-
-  server.use(async (ctx, next) => {
-    ctx.res.statusCode = 200;
     await next();
   });
 
